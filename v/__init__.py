4b555131afa21607d7e58ba501b2eaededf529d6
pumingzheng <pumz_1991@126.com>
2023-03-13 16:22:24 +0800

    fix([BUILD]): aaa

--- v/__init__.py
+++
@@ -1,8 +0,0 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("easy_tools")
-except PackageNotFoundError:
-    __version__ = "unknown version"
-
-print(__version__)
